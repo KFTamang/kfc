189ddebb2ac1f5493d5a6868aae28b11461af810
KFTamang <subsonic.detonation@gmail.com>
2020-04-05 23:43:55 +0900

    add header file before compiling for selfhosting

--- selfhost.sh
+++ selfhost.sh
@@ -6,10 +6,13 @@ for src in parse.c;do
 tmp=sh_${src/.c/_1.c}
 asm=sh_${src/.c/_1.s}
 obj=sh_${src/.c/_1.o}
-cat kfc.h ${src} > ${tmp}
+cat ${src/.c/.h} ${src} > tmp
+cat kfc.h tmp > ${tmp}
 sed -i 's/, \.\.\.//g' ${tmp}
 ./kfc ${tmp} > ${asm}
 gcc ${asm} -o ${obj}
 done
+
+rm tmp
 
 # gcc ${srcs} -o kfc_gen1
