c591793fc10e41a52c2c2cf4bdf65ea4bc7cd1dd
KFTamang <subsonic.detonation@gmail.com>
2020-10-26 14:27:03 +0900

    modify selfhost.sh to compile other source files

--- selfhost.sh
+++ selfhost.sh
@@ -2,7 +2,7 @@
 
 srcs=(parse.c tokenizer.c container.c codegen.c main.c)
 
-for src in parse.c;do
+for src in ${srcs[@]};do
 tmp=sh_${src/.c/_1.c}
 asm=sh_${src/.c/_1.s}
 obj=sh_${src/.c/_1.o}
@@ -10,9 +10,10 @@ cat ${src/.c/.h} ${src} > tmp
 cat kfc.h tmp > ${tmp}
 sed -i 's/, \.\.\.//g' ${tmp}
 ./kfc ${tmp} > ${asm}
-gcc ${asm} -o ${obj}
+gcc -c ${asm} -o ${obj}
 done
 
 rm tmp
 
-# gcc ${srcs} -o kfc_gen1
+# objs=(sh_parse_1.o sh_tokenizer_1.o sh_container_1.o sh_codegen_1.o sh_main_1.o)
+# gcc ${objs} -o kfc_gen1
