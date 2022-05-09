b73a619e8e488e12f492b5ed36930fffa0091619
KFTamang <subsonic.detonation@gmail.com>
2022-05-10 00:21:09 +0900

    use gcc preprocessor for selfhosting script

--- selfhost.sh
+++ selfhost.sh
@@ -4,26 +4,29 @@ srcs1=(parse.c codegen.c)
 srcs2=(tokenizer.c container.c main.c)
 
 function preprocess(){
-    header=${1/.c/.h}
-    if [ $2 = 1 ];then
-        cat $header $1 > tmp.c
-    else
-        cat $1 > tmp.c
-    fi
-    cat kfc.h tmp.c > result.c
-    sed -i 's/, \.\.\.//g' result.c # ignore variable length argument function
-    sed -i 's/VAR_NAME_SIZE/512/g' result.c
-    sed -i 's/MAX_ARG_NUM/6/g' result.c 
-    sed -i 's/MAX_SYMBOL_NUM/65535/g' result.c
-    sed -i 's/stderr/2/g' result.c
-    sed -i 's/va_start(ap, fmt)/ap=\&(fmt)+sizeof(fmt)/g' result.c
-    sed -i 's/va_list/void*/g' result.c
-    sed -i 's/FILE*/void*/g' result.c
-    sed -i 's/EXIT_FAILURE/1/g' result.c
-    sed -i 's/SEEK_END/2/g' result.c
-    sed -i 's/SEEK_SET/0/g' result.c
-    cat result.c
-    rm tmp.c result.c
+    # header=${1/.c/.h}
+    # echo "" > tmp_header.h
+    # echo "extern FILE* stdout;" >> tmp_header.h
+    # echo "extern FILE* stderr;" >> tmp_header.h
+    # if [ $2 = 1 ];then
+    #     cat $header tmp_header.h $1 > tmp.c
+    # else
+    #     cat tmp_header.h $1 > tmp.c
+    # fi
+    # cat kfc.h tmp.c > result.c
+    # sed -i 's/, \.\.\.//g' result.c # ignore variable length argument function
+    # sed -i 's/VAR_NAME_SIZE/512/g' result.c
+    # sed -i 's/MAX_ARG_NUM/6/g' result.c 
+    # sed -i 's/MAX_SYMBOL_NUM/65535/g' result.c
+    # sed -i 's/va_start(ap, fmt)/ap=\&(fmt)+sizeof(fmt)/g' result.c
+    # sed -i 's/va_list/void*/g' result.c
+    # sed -i 's/FILE*/void*/g' result.c
+    # sed -i 's/EXIT_FAILURE/1/g' result.c
+    # sed -i 's/SEEK_END/2/g' result.c
+    # sed -i 's/SEEK_SET/0/g' result.c
+    # cat result.c
+    # rm tmp.c result.c tmp_header.h
+    gcc -E $1
 }
 
 for src in ${srcs1[@]};do
@@ -42,6 +45,7 @@ preprocess ${src} "0" > ${tmp}
 done
 
 asms=(sh_main_1.s sh_parse_1.s sh_tokenizer_1.s sh_container_1.s sh_codegen_1.s)
-gcc -no-pie -o kfc_gen1 ${asms[@]} -std=c11 -g
+gcc -no-pie -o kfc_gen1 ${asms[@]} -std=c11 -g -static
 # echo ${asms[@]}
 
+objdump -D kfc_gen1 > kfc_gen1.s
